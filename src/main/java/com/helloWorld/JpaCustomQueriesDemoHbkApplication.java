6485a30017c26dd5a6fb6fc92479bd4cb33a8fb8
Thahir Thavvagunta <ahmedthahir369@gmail.com>
2022-07-16 11:58:24 +0300

    initial code setup

---
+++ src/main/java/com/helloWorld/JpaCustomQueriesDemoHbkApplication.java
@@ -0,0 +1,13 @@
+package com.helloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCustomQueriesDemoHbkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaCustomQueriesDemoHbkApplication.class, args);
+	}
+
+}
